62f85c9ef48b3c815766e0008b93cecd8a9b46db
sjGwon <gwon@gmail.com>
2019-05-08 20:07:38 +0900

    Revert "ActionForward-ridirect 추가"
    This reverts commit 65daacc8b163c8c0924583034de5a8621b6b42f7.

--- TR_Project_Team2/src/kr/or/bit/action/ActionForward.java
+++ TR_Project_Team2/src/kr/or/bit/action/ActionForward.java
@@ -2,14 +2,7 @@ package kr.or.bit.action;
 
 public class ActionForward {
 	private String path=null;//이동 경로
-	private boolean isRedirect = false;
-	
-		public boolean isRedirect() {
-		return isRedirect;
-	}
-	public void setRedirect(boolean isRedirect) {
-		this.isRedirect = isRedirect;
-	}
+		
 		public String getPath() {
 			return path;
 		}
