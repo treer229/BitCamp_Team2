aa810dbf7d4e00d4529a73fa861c93fb4ccdd053
이힘찬 <glacks4871@gmail.com>
2019-05-09 16:40:20 +0900

    lib/json-lib-2.4 추가 , TravelReview Sevice 추가

---
+++ TR_Project_Team2/src/kr/or/bit/service/TravelReviewEditOkService.java
@@ -0,0 +1,45 @@
+package kr.or.bit.service;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.bit.action.Action;
+import kr.or.bit.action.ActionForward;
+import kr.or.bit.dao.TravelReviewDao;
+
+public class TravelReviewEditOkService implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//request.setCharacterEncoding("utf-8");
+		
+		String idx = request.getParameter("idx");
+		String msg = "";
+		String url ="";
+		PrintWriter out = response.getWriter();
+		
+		if(idx == null || idx.trim().equals("")){
+			out.print("<script>");
+			out.print("alert('글번호 오류');");
+			out.print("location.href='board_list.jsp'");	   
+			out.print("</script>");
+		}
+		//boardservice service = boardservice.getInstance();
+		//int result = service.board_edit(request);
+		
+		TravelReviewDao dao = new TravelReviewDao();
+		int result = dao.boardedit(request);
+		
+		ActionForward forward = new ActionForward();
+	    request.setAttribute("result", result);
+	    request.setAttribute("idx", idx);
+	    
+	    forward.setRedirect(false);
+		forward.setPath("/board/board_editok.jsp");
+		
+		return forward;
+	}
+
+}
