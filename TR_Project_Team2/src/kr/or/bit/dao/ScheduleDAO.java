2906bbef0eb6545b0ccb2456d0c72477971d5c2e
hoguma <bnbn2400@naver.com>
2019-05-09 18:02:06 +0900

    2019/05/09 서비스 이름수정 체크리스트 and 캘린더 서비스 CRUD 구현 완료-

--- TR_Project_Team2/src/kr/or/bit/dao/ScheduleDAO.java
+++ TR_Project_Team2/src/kr/or/bit/dao/ScheduleDAO.java
@@ -33,7 +33,7 @@ public class ScheduleDAO {
 			System.out.println("look yp Fail"+ e.getMessage());
 		}
 	}
-	public List<Schedule> getScheduleList(String id) {
+	public List<Schedule> getScheduleListAll(String id) {
 		
 		List<Schedule> list = new ArrayList<Schedule>();
 		try {
@@ -57,6 +57,41 @@ public class ScheduleDAO {
 				schedule.setCompleteok(rs.getInt(9));
 				list.add(schedule);
 			}
+			System.out.println("스케줄 전체 쿼리 담기 완료");
+		} catch (Exception e) {
+			System.out.println("스케줄 전체 불러오기 에러");
+			System.out.println(e.getMessage());
+		}finally {
+			if(rs!=null) try {rs.close();}catch(Exception e) {System.out.println("스케줄전체 rs DB서버 닫기 실패"); System.out.println(e.getMessage());}
+    	  	if(pstmt!=null) try{pstmt.close();}catch (Exception e){System.out.println("스케줄전체 pstmt DB서버 닫기 실패"); System.out.println(e.getMessage());}
+			if(conn!=null) try{conn.close();}catch (Exception e){System.out.println("스케줄전체 conn DB서버 닫기 실패"); System.out.println(e.getMessage());}
+	}
+		return list;
+	}
+	public Schedule getScheduleList(int schedule_num) {
+		Schedule schedule = null;
+		try {
+			conn = ds.getConnection();
+			String sql = "select * from SCHEDULE where schedule_num = ? ORDER BY SCHEDULE_NUM DESC";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, schedule_num);
+			
+			rs = pstmt.executeQuery();
+			System.out.println("스케줄 불러오는 쿼리문 실행 완료");
+			if(rs.next()) {
+				schedule = new Schedule();
+				schedule.setSchedule_num(rs.getInt(1));
+				schedule.setId(rs.getString(2));
+				schedule.setSchedule_title(rs.getString(3));
+				schedule.setContent(rs.getString(4));
+				schedule.setSchedule_start(rs.getString(5));
+				schedule.setSchedule_end(rs.getString(6));
+				schedule.setColor(rs.getString(7));
+				schedule.setDeleteok(rs.getInt(8));
+				schedule.setCompleteok(rs.getInt(9));
+			}else {
+				System.out.println("Rs실행 실패 [스케줄 단일검색]");
+			}
 			System.out.println("스케줄 쿼리 담기 완료");
 		} catch (Exception e) {
 			System.out.println("스케줄 불러오기 에러");
@@ -66,7 +101,7 @@ public class ScheduleDAO {
     	  	if(pstmt!=null) try{pstmt.close();}catch (Exception e){System.out.println("스케줄 pstmt DB서버 닫기 실패"); System.out.println(e.getMessage());}
 			if(conn!=null) try{conn.close();}catch (Exception e){System.out.println("스케줄 conn DB서버 닫기 실패"); System.out.println(e.getMessage());}
 	}
-		return list;
+		return schedule;
 	}
 	public int getInsertSchedule(Schedule schedule) {
 		int num =0;
@@ -123,7 +158,7 @@ public class ScheduleDAO {
 		int row = 0;
 		try {
 			conn = ds.getConnection();
-			String sql = "update SCHEDULE set SCHEDULE_TITLE=? , CONTENT=? , SCHEDULE_START=? , SCHEDULE_END=? , COLOR,DELETEOK=? ,COMPLETEOK=? where SCHEDULE_NUM = ?" ;
+			String sql = "update SCHEDULE set SCHEDULE_TITLE=? , CONTENT=? , SCHEDULE_START=? , SCHEDULE_END=? , COLOR=? ,DELETEOK=? ,COMPLETEOK=? where SCHEDULE_NUM = ?" ;
 			pstmt = conn.prepareStatement(sql);
 			
 			pstmt.setString(1, schedule.getSchedule_title());
@@ -134,7 +169,7 @@ public class ScheduleDAO {
 			pstmt.setInt(6, schedule.getDeleteok());
 			pstmt.setInt(7, schedule.getCompleteok());
 			pstmt.setInt(8, schedule.getSchedule_num());
-			
+			System.out.println("담긴 데이터 정보 :" + schedule);
 			row = pstmt.executeUpdate();
 			
 			if(row > 0) {
