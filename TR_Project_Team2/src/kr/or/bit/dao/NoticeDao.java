95cb209c9930afa4b9401837bff557369636e883
sjGwon <gwon@gmail.com>
2019-05-09 14:11:52 +0900

    NoticeDao 작업 마무리

--- TR_Project_Team2/src/kr/or/bit/dao/NoticeDao.java
+++ TR_Project_Team2/src/kr/or/bit/dao/NoticeDao.java
@@ -10,7 +10,6 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import kr.or.bit.dto.Notice;
-import kr.or.kosta.dto.board;
 
 public class NoticeDao {
 	
@@ -34,7 +33,7 @@ public class NoticeDao {
 		}
 	}
 	
-	public int noticeWriteOk(Notice notice) {//일반 게시판 글쓰기 작업
+	public int noticeWrite(Notice notice) {//일반 게시판 글쓰기 작업
 		int row = 0;
 		try {
 			conn = ds.getConnection();
@@ -65,7 +64,7 @@ public class NoticeDao {
 		Notice notice = null;
 		try{
 				conn = ds.getConnection();
-				String sql ="select * from jspboard where notice_num=?";
+				String sql ="select * from NOTICE where notice_num=?";
 			  	pstmt = conn.prepareStatement(sql);
 			  	pstmt.setInt(1, notice_num);
 			  			  	
@@ -93,72 +92,48 @@ public class NoticeDao {
 		    }
 		 return notice;
 	}	
-	public int reWriteOk(int notice_num, Notice notice) {
+	
+	
+	public int updateNotice(int notice_num, Notice notice) {//title과 content를 수정한다.
+		int row = 0;
 		try{
+			String sql = "update NOTICE set title=?,content=?  where=noticce_num=?";
 			conn = ds.getConnection();
-			//사용자 입력값 처리
-			String sql = "update NOTICE set()  where=?";
+			pstmt.setString(1, notice.getnotice_title());
+			pstmt.setString(2, notice.getnotice_content());
+			pstmt.setInt(3, notice_num);
 			
-			int idx = boardata.getIdx(); //추가
-			String writer = boardata.getWriter();
-			String email = boardata.getEmail();
-			String homepage = boardata.getHomepage();
-			String pwd = boardata.getPwd();
-			String subject = boardata.getSubject();
-			String content = boardata.getContent();
-			String filename = boardata.getFilename();
-			int filesize = 0;
+			row = pstmt.executeUpdate();
 			
 			
-			
-			
-			//refer , depth , step 값 가져오기
-			pstmt = conn.prepareStatement(refer_depth_step_sql);
-			pstmt.setInt(1, idx);
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()){
-				int refer = rs.getInt("refer");
-				int step = rs.getInt("step");
-				int depth = rs.getInt("depth");
-				
-				//step () 값 업데이트
-			
-				pstmt = conn.prepareStatement(step_update_sql);
-				pstmt.setInt(1, step);
-				pstmt.setInt(2, refer);
-				pstmt.executeUpdate();
-				
-			    //실 데이터 insert 
-				//jspboard_idx.nextval,?,?,?,?,?,?,?,?,0,?,?,?
-				      
-				                  
-				pstmt = conn.prepareStatement(rewrite_sql);
-				pstmt.setString(1, writer);
-				pstmt.setString(2, email);
-				pstmt.setString(3, homepage);
-				pstmt.setString(4, pwd);
-				pstmt.setString(5, subject);
-				pstmt.setString(6,content);
-				pstmt.setInt(7, 0);
-				//첨부파일
-				pstmt.setString(8, filename);
-				//답변형
-				pstmt.setInt(9, refer);
-				pstmt.setInt(10, depth+1);   //답글 처리
-				pstmt.setInt(11, step+1); //답글처리 (현재 읽은 글보다 큰 순번은 + 1)
-				
-				int row = pstmt.executeUpdate();
-				return row;
-			}else{
-				return -1;
-			}
-				
+		}catch (Exception e) {
+			e.getStackTrace();
+			row = -1;
 		}finally{
-			if(pstmt != null) pstmt.close();
-			if(rs != null) rs.close();
-			if(conn != null) conn.close();
+			if (pstmt != null) try {pstmt.close();} catch (Exception e) {}
+			if (conn != null) try {conn.close();} catch (Exception e) {}
 		}
-		
+		return row;
 	}
+	
+	public int deleteNotice(int num) {
+		  int row = 0;
+	      PreparedStatement pstmt = null;
+
+	      try{
+	         conn = ds.getConnection();
+	         String sql = "delete from NOTICE where NOTICE_NUM=?";
+	         pstmt = conn.prepareStatement(sql);
+	         pstmt.setInt(1, num);
+	         row = pstmt.executeUpdate();
+
+	      }catch(Exception e){
+	         System.out.println(e.getMessage());
+	      }finally {
+		    if(pstmt!=null) try{pstmt.close();}catch (Exception e){}
+			if(conn!=null) try{conn.close();}catch (Exception e){} //반환
+	      }
+		return row;
+	}
+	
 }
