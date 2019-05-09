3070d663550587f02296ae4160121e1131237c58
이힘찬 <glacks4871@gmail.com>
2019-05-09 11:15:45 +0900

    travelreview Dao 수정,  qna TR reply  dto 삭제

--- TR_Project_Team2/src/kr/or/bit/dto/TravelReview.java
+++ TR_Project_Team2/src/kr/or/bit/dto/TravelReview.java
@@ -16,9 +16,9 @@ public class TravelReview {			//여행후기
 	private int comment_count;		//댓글수
 	
 	// 계층형
-	private int refer; 				// 참조
-	private int depth; 				// 들여쓰기
-	private int step; 				// 글의 순서
+	private int ref; 				// 참조
+	private int dep; 				// 들여쓰기
+	private int stp; 				// 글의 순서
 	
 	
 	public TravelReview() {
@@ -26,7 +26,7 @@ public class TravelReview {			//여행후기
 	}
 	
 	public TravelReview(int tr_num, String id, String tr_title, String content, int views, String created_date2,
-			int comment_count, int refer, int depth, int step) {
+			int comment_count, int ref, int dep, int stp) {
 		this.tr_num = tr_num;
 		this.id = id;
 		this.tr_title = tr_title;
@@ -34,58 +34,89 @@ public class TravelReview {			//여행후기
 		this.views = views;
 		this.created_date = created_date2;
 		this.comment_count = comment_count;
-		this.refer = refer;
-		this.depth = depth;
-		this.step = step;
+		this.ref = ref;
+		this.dep = dep;
+		this.stp = stp;
 	}
-	
+
 	public int getTr_num() {
 		return tr_num;
 	}
+
 	public void setTr_num(int tr_num) {
 		this.tr_num = tr_num;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getTr_title() {
 		return tr_title;
 	}
+
 	public void setTr_title(String tr_title) {
 		this.tr_title = tr_title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public int getViews() {
 		return views;
 	}
+
 	public void setViews(int views) {
 		this.views = views;
 	}
+
 	public String getCreated_date() {
 		return created_date;
 	}
+
 	public void setCreated_date(String created_date) {
 		this.created_date = created_date;
 	}
+
 	public int getComment_count() {
 		return comment_count;
 	}
+
 	public void setComment_count(int comment_count) {
 		this.comment_count = comment_count;
 	}
-	
-	@Override
-	public String toString() {
-		return "TravelReview [tr_num=" + tr_num + ", id=" + id + ", tr_title=" + tr_title + ", content=" + content
-				+ ", views=" + views + ", created_date=" + created_date + ", comment_count=" + comment_count + "]";
+
+	public int getRef() {
+		return ref;
+	}
+
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+
+	public int getDep() {
+		return dep;
+	}
+
+	public void setDep(int dep) {
+		this.dep = dep;
+	}
+
+	public int getStp() {
+		return stp;
+	}
+
+	public void setStp(int stp) {
+		this.stp = stp;
 	}
 	
 }
