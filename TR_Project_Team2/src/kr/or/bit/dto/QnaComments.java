58c43422aaba5a43b237ea0d1a693090d077cd2c
sjGwon <gwon@gmail.com>
2019-05-09 11:06:48 +0900

    dto삭제 : Qna_Reply, Qna_Reply_Comments,Tr_Reply_Comments, TravelReview_Reply 파일명변경 : Qna_Comments -> QnaComments, Travel_Comments -> TravelComments dto 수정 : Notice -> id추가

--- TR_Project_Team2/src/kr/or/bit/dto/QnaComments.java
+++ TR_Project_Team2/src/kr/or/bit/dto/QnaComments.java
@@ -1,87 +1,65 @@
 package kr.or.bit.dto;
 
-public class Tr_Comments {
+public class QnaComments {
 	
 	/*
     작업일자 : 2019-05-08
     작업자 :    권순조
-    작업내용 : 여행리뷰 댓글 dto 작성
+    작업내용 : QNA댓글 DTO  작성
     */
 	
 	private int comments_num;
 	private String id;
-	private int tr_num;
+	private int qna_num;
 	private String comments_content;
 	private String comments_date;
 	
-	
-	
-	public int getComments_num() {
+	public int getcomments_num() {
 		return comments_num;
 	}
 
-
-
-	public void setComments_num(int comments_num) {
+	public void setcomments_num(int comments_num) {
 		this.comments_num = comments_num;
 	}
-
-
 
 	public String getId() {
 		return id;
 	}
 
-
-
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
-
-	public int getTr_num() {
-		return tr_num;
+	public int getQna_num() {
+		return qna_num;
 	}
 
-
-
-	public void setTr_num(int tr_num) {
-		this.tr_num = tr_num;
+	public void setQna_num(int qna_num) {
+		this.qna_num = qna_num;
 	}
-
-
 
 	public String getComments_content() {
 		return comments_content;
 	}
 
-
-
 	public void setComments_content(String comments_content) {
 		this.comments_content = comments_content;
 	}
 
-
-
-	public String getComments_date() {
+	public String getcomments_date() {
 		return comments_date;
+	
 	}
 
-
-
-	public void setComments_date(String comments_date) {
+	public void setcomments_date(String comments_date) {
 		this.comments_date = comments_date;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "tr_Comments [comments_num=" + comments_num + ", id=" + id + ", tr_num=" + tr_num + ", comments_content="
-				+ comments_content + ", comments_date=" + comments_date + "]";
+		return "Qna_Comments [comments_num=" + comments_num + ", id=" + id + ", qna_num=" + qna_num
+				+ ", comments_content=" + comments_content + ", comments_date=" + comments_date + "]";
 	}
 
-	
 	
 }
