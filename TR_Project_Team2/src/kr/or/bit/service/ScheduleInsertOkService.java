9955da59ac976f0b0c3681315c6757a71978851a
hoguma <bnbn2400@naver.com>
2019-05-10 13:47:16 +0900

    2019/05/10 체크리스트,컨텐츠 서비스 완료

--- TR_Project_Team2/src/kr/or/bit/service/ScheduleInsertOkService.java
+++ TR_Project_Team2/src/kr/or/bit/service/ScheduleInsertOkService.java
@@ -8,27 +8,24 @@ import kr.or.bit.action.ActionForward;
 import kr.or.bit.dao.ScheduleDAO;
 import kr.or.bit.dto.Schedule;
 
-public class ScheduleEditOk implements Action {
+public class ScheduleInsertOkService implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ActionForward forward = new ActionForward();
 		
-		int num = Integer.parseInt(request.getParameter("num"));
-		String id =	request.getParameter("id");
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		String start =	request.getParameter("start");
-		String end =	request.getParameter("end");
-		String color =	request.getParameter("color");
-		int del = Integer.parseInt(request.getParameter("del"));
-		int com = Integer.parseInt(request.getParameter("com"));
-		
+	String id =	request.getParameter("id");
+	String title = request.getParameter("title");
+	String content = request.getParameter("content");
+	String start =	request.getParameter("start");
+	String end =	request.getParameter("end");
+	String color =	request.getParameter("color");
+	int del = Integer.parseInt(request.getParameter("del"));
+	int com = Integer.parseInt(request.getParameter("com"));
 		
 		ScheduleDAO dao = new ScheduleDAO();
 		Schedule sc = new Schedule();
 		
-		sc.setSchedule_num(num);
 		sc.setId(id);
 		sc.setSchedule_title(title);
 		sc.setContent(content);
@@ -38,9 +35,13 @@ public class ScheduleEditOk implements Action {
 		sc.setDeleteok(del);
 		sc.setCompleteok(com);
 		
-		dao.getUpdeateSchedule(sc);
+		dao.getInsertSchedule(sc);
 		
-		forward.setPath("/list.Schedule");
+		request.setAttribute("id", id);
+		
+		forward.setRedirect(true);
+		forward.setPath("list.Schedule");
+		
 		return forward;
 	}
 
