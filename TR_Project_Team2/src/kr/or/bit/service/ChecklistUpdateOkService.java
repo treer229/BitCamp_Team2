f6d63c5b8fb1b45978f97d9899fe651116b6c5b0
hoguma <bnbn2400@naver.com>
2019-05-10 13:47:16 +0900

    2019/05/10 체크리스트,컨텐츠 서비스 완료

--- TR_Project_Team2/src/kr/or/bit/service/ChecklistUpdateOkService.java
+++ TR_Project_Team2/src/kr/or/bit/service/ChecklistUpdateOkService.java
@@ -8,7 +8,7 @@ import kr.or.bit.action.ActionForward;
 import kr.or.bit.dao.ChecklistDAO;
 import kr.or.bit.dto.Checklist;
 
-public class ChecklistUpdateOk implements Action {
+public class ChecklistUpdateOkService implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
