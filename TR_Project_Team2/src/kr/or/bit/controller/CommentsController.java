30823495bd09722ab1569a2f14366737373dbd93
sjGwon <gwon@gmail.com>
2019-05-10 11:49:22 +0900

    공지 댓글 서비스 및 컨트롤러 1차 완료

--- TR_Project_Team2/src/kr/or/bit/controller/CommentsController.java
+++ TR_Project_Team2/src/kr/or/bit/controller/CommentsController.java
@@ -9,6 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
+<<<<<<< HEAD
+=======
+import kr.or.bit.service.CommentsListService;
+import kr.or.bit.service.CommentsUpdateOkService;
+import kr.or.bit.service.CommentsUpdateService;
+import kr.or.bit.service.CommentsDeleteService;
+import kr.or.bit.service.CommentsInsertService;
+>>>>>>> 공지 댓글 서비스 및 컨트롤러 1차 완료
 
 /**
  * Servlet implementation class CommentsController
@@ -40,10 +48,48 @@ public class CommentsController extends HttpServlet {
     	
     	if(url_Command.equals("/InsertNotice.Comments")) {// 공지사항 댓글 보기
     		
+<<<<<<< HEAD
     		
     	}  else if(url_Command.equals("/MemoList.do")) {
     //		forward = new ActionForward();
     //		forward.setPath(/*여기에 주소를 넣어주세요*/);
+=======
+    	}  else if(url_Command.equals("/noticomlist.Comments")) {
+
+    		System.out.println("noticomlist.Comments");
+    		action = new CommentsListService();
+      	  	try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				System.out.println("컨트롤러 막힘");
+				e.printStackTrace();
+			}
+    	} else if(url_Command.equals("/DeleteComNotice.comments")) {
+    		System.out.println("DeleteComNotice");
+    		action = new CommentsDeleteService();
+    		try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				System.out.println("컨트롤러막힘");
+				e.printStackTrace();
+			}
+    	} else if(url_Command.equals("/UpdateComNotice.comments")) {
+    		try {
+        		System.out.println("UpdateComNotice");
+        		action = new CommentsUpdateService();
+			} catch (Exception e) {
+				System.out.println("컨트롤러 막힘");
+				e.printStackTrace();
+			}
+    	} else if(url_Command.equals("/UpdateOkComNotice.comments")) {
+    		try {
+        		System.out.println("UpdateOkComNotice");
+        		action = new CommentsUpdateOkService();
+			} catch (Exception e) {
+				System.out.println("컨트롤러 막힘");
+				e.printStackTrace();
+			}
+>>>>>>> 공지 댓글 서비스 및 컨트롤러 1차 완료
     	}
     }
 		
